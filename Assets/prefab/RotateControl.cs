9c92512b5b4394b478f792f6fb7baba3bda5db31
Saito Hughes <julijaraupb5.1@gmail.com>
2021-05-16 18:27:51 -0500

    Trabajo particula Escudo

---
+++ Assets/prefab/RotateControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] AnimationCurve myCurve;
+    Light pointlight;
+    Renderer myRend;
+    [SerializeField] float speed, intesity, alpha, duration;
+    float t=0;
+    Color prevColor;
+
+    void Awake()
+    {
+        pointlight = GetComponentInChildren<Light>();
+        myRend = GetComponentInChildren<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        prevColor = myRend.material.color;
+        float y = myCurve.Evaluate(t / duration);
+        myRend.material.color = new Color(prevColor.r, prevColor.g, prevColor.b, y*alpha);
+        
+        //if (y > alpha)
+        //{
+        //    myRend.material.color = new Color(prevColor.r, prevColor.g, prevColor.b, alpha);
+        //}
+        pointlight.intensity = y*intesity;
+
+        t += Time.deltaTime;
+
+        //transform.RotateAround(transform.position, Vector3.down, -speed*Time.deltaTime);
+
+        
+    }
+}
