be3457df330c4d538f3f054acad3104682ea334b
JuanCalvache <47004448+JuanCalvache@users.noreply.github.com>
2021-05-17 18:04:14 -0500

    mejoras epicardas en el escudo

--- Assets/prefab/RotateControl.cs
+++ Assets/prefab/RotateControl.cs
@@ -6,17 +6,27 @@ public class RotateControl : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] AnimationCurve myCurve;
+    [SerializeField] AnimationCurve orbitalCurve;
+    [SerializeField] AnimationCurve textureCurve;
+    [SerializeField] AnimationCurve emisionCurve;
     Light pointlight;
     Renderer myRend;
-    [SerializeField] float speed, intesity, alpha, duration;
+    [SerializeField] float speed, intesity, alpha, duration, maxOrbitalSpeed, maxEmision;
     float t=0;
     Color prevColor;
+    ParticleSystem particle;
+    ParticleSystem.VelocityOverLifetimeModule velOverlifeTime;
+    ParticleSystem.EmissionModule emisionModule;
+    float xOrbitalSpeed, orbitalTextureSpeed;
+    [SerializeField] Transform ball;
 
     void Awake()
     {
         pointlight = GetComponentInChildren<Light>();
         myRend = GetComponentInChildren<Renderer>();
-
+        particle = GetComponentInChildren<ParticleSystem>();
+        velOverlifeTime = particle.velocityOverLifetime;
+        emisionModule = particle.emission;
     }
 
     // Update is called once per frame
@@ -25,17 +35,16 @@ public class RotateControl : MonoBehaviour
         prevColor = myRend.material.color;
         float y = myCurve.Evaluate(t / duration);
         myRend.material.color = new Color(prevColor.r, prevColor.g, prevColor.b, y*alpha);
-        
-        //if (y > alpha)
-        //{
-        //    myRend.material.color = new Color(prevColor.r, prevColor.g, prevColor.b, alpha);
-        //}
+        xOrbitalSpeed = orbitalCurve.Evaluate(t / duration) * maxOrbitalSpeed;
+        orbitalTextureSpeed = myCurve.Evaluate(t / duration);
+        velOverlifeTime.orbitalY = xOrbitalSpeed;
+        emisionModule.rateOverTime = emisionCurve.Evaluate(t / duration) * maxEmision;
+        ball.RotateAround(transform.position, Vector3.down, orbitalTextureSpeed*-speed * Time.deltaTime);
         pointlight.intensity = y*intesity;
-
         t += Time.deltaTime;
-
-        //transform.RotateAround(transform.position, Vector3.down, -speed*Time.deltaTime);
-
-        
+        if (y > alpha)
+        {
+            myRend.material.color = new Color(prevColor.r, prevColor.g, prevColor.b, alpha);
+        }
     }
 }
---
+++ Assets/shaders/TexturaMoviendose.shader
@@ -0,0 +1,57 @@
+﻿// Directorio /Nombre del shader
+Shader "Custom/Base/MovText" {
+
+	// Variables disponibles en el inspector (Propiedades)
+	Properties { 
+		[HDR]_ColorAmbiente ("Color Ambiente", Color) = (1,1,1,1)
+		_Texture("Textura Base",2D) = "white"{}
+		_Speed("Velocidad",Vector) = (0,0,0,0)
+		_Factor("Factor", Range(0,1)) = 0
+	}
+
+	// Primer subshader
+	SubShader { 
+		Tags {"Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "TransparentCutOut"}
+		//ZWrite Off
+		//Blend SrcAlpha OneMinusSrcAlpha
+		LOD 100
+		
+		CGPROGRAM
+		// Método para el cálculo de la luz
+		#pragma surface surf Standard fullforwardshadows Alpha:fade
+		#pragma target 3.0
+
+		// Información adicional provista por el juego
+		struct Input {
+			float2 uv_Texture;
+			
+		};
+
+		// Declaración de variables
+		float4 _ColorAmbiente;
+		float4 _Speed;
+		sampler2D _Texture;
+		float _Factor;
+
+		// Nucleo del programa
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+
+			float2 Uv = IN.uv_Texture;
+			float distanciaX = _Speed.x * _Time.y;
+			float distanciaY = _Speed.y * _Time.y;
+			Uv += float2 (distanciaX, distanciaY);
+			float4 output = tex2D(_Texture, Uv);
+			o.Albedo = (output * _ColorAmbiente).rgb;
+			output.a = _Factor;
+			o.Alpha = output.a;
+		}
+		ENDCG
+
+	}// Final del primer subshader
+
+	// Segundo subshader si existe alguno
+	// Tercer subshader...
+
+	// Si no es posible ejecutar ningún subshader ejecute Diffuse
+	FallBack "Diffuse"
+}
