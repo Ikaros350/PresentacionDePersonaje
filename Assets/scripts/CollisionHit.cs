3c6749425e48f27d4af9849b0b6aa8b031ac31c5
Saito Hughes <julijaraupb5.1@gmail.com>
2021-05-23 20:07:20 -0500

    Escena2 lista

---
+++ Assets/scripts/CollisionHit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHit : MonoBehaviour
+{
+    Animator myAnim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myAnim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Fire"))
+        {
+
+            myAnim.SetBool("Frozen", false);
+        }
+        if (collision.gameObject.CompareTag("Ice"))
+        {
+            myAnim.SetBool("Frozen", true);
+        }  
+    }
+}
